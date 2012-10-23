b739506886ca0c7d5a4b08d586f04235fa0fa84b
stef <stefan.marsiske@gmail.com>
2012-10-23 22:49:16 +0200

    [enh] limit incomming msgs to 4KB

--- tget.sh
+++ tget.sh
@@ -2,14 +2,18 @@
 
 # helper used by tmsg.sh - checks and displays valid incoming messages
 
-cfgdir=.
-read header
-match=false
-cat "$cfgdir"/peers | while read name host key; do
-    ts=$(echo -n "$header" | seccure-verify -q -a -c p521 -m 256 -f "$key" 2>${logfile:-/dev/null}) && {
-        printf "$ts <$name> "
-        match=true
-        break
-    }
-done
-$match && seccure-verify -q -a -c p521 -m 256 "$key" -f
+# let's not allow any resource exhaustion because of overly big messages
+# 4K should be enough for chatting
+dd bs=1024 count=4 2>/dev/null | {
+    cfgdir=.
+    read header
+    match=false
+    cat "$cfgdir"/peers | while read name host key; do
+        ts=$(echo -n "$header" | seccure-verify -q -a -c p521 -m 256 -f "$key" 2>${logfile:-/dev/null}) && {
+            printf "$ts <$name> "
+            match=true
+            break
+        }
+    done
+    $match && seccure-verify -q -a -c p521 -m 256 "$key" -f
+}
